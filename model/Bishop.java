67f0da6eaa15799fb35559686de81d12344ee474
Asuka <tranhung2642005@gmail.com>
2025-12-11 09:56:51 +0700

    Add files via upload
    hihi

---
+++ model/Bishop.java
@@ -0,0 +1,49 @@
+package model;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bishop extends Piece {
+
+    public Bishop(PieceColor color, int row, int col) {
+        super(color, row, col);
+    }
+
+    @Override
+    public char getSymbol() {
+        return color == PieceColor.WHITE ? 'B' : 'b';
+    }
+
+    @Override
+    public List<Position> getPossibleMoves(Board board) {
+        List<Position> moves = new ArrayList<>();
+
+        // 4 hướng chéo: (-1,-1), (-1,+1), (+1,-1), (+1,+1)
+        int[] dr = {-1, -1, 1, 1};
+        int[] dc = {-1, 1, -1, 1};
+
+        for (int i = 0; i < 4; i++) {
+            int r = row + dr[i];
+            int c = col + dc[i];
+
+            while (inBounds(r, c)) {
+                if (board.isEmpty(r, c)) {
+                    moves.add(new Position(r, c));
+                } else {
+                    if (board.getPiece(r, c).getColor() != this.color) {
+                        moves.add(new Position(r, c)); // ăn quân đối phương
+                    }
+                    break; // gặp quân → dừng hướng đó
+                }
+                r += dr[i];
+                c += dc[i];
+            }
+        }
+
+        return moves;
+    }
+
+	@Override
+	public Piece clonePiece() {
+		 return new Bishop(this.color, this.row, this.col);
+	}
+}
